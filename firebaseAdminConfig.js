76126f6d237efb9dd77d50d7e5ea02f15ee2aacb
hubielu <luhubert@stanford.edu>
2025-01-03 02:36:45 -0600

    Fixing API access from domain

--- firebaseAdminConfig.js
+++ firebaseAdminConfig.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 // Replace this with the path to your Firebase service account key file
-const serviceAccount = require('/Users/hubert/Downloads/network-insider-firebase-adminsdk-4axco-ce029cd370.json');
+const serviceAccount = require('./network-insider-firebase-adminsdk-4axco-ce029cd370.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
