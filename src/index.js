b503684e2e167b2a125c95bacf712436d56e7254
hubielu <luhubert@stanford.edu>
2025-01-05 03:09:04 -0800

    Implemented lazy loading and code splittingk k thuird try

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
+import './App.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-serviceWorker.register();
+root.render(<App />);
