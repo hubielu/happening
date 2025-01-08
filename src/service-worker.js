6ba02472a0600e2e77f0111652c508d6c30ba14c
hubielu <luhubert@stanford.edu>
2025-01-07 23:39:13 -0800

    finally??

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-restricted-globals */
 
+
+import { precacheAndRoute } from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
 const CACHE_NAME = 'network-insider-cache-v1';
 const urlsToCache = [
   '/',
